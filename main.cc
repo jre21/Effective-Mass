34c4f9b8d917e723ef3a93c16999a00c9f9e3517
Jacob Emmert-Aronson <jre21@case.edu>
2012-04-11 20:41:18 -0400

    main.cc: update for new tests
    Signed-off-by: Jacob Emmert-Aronson <jre21@case.edu>

--- main.cc
+++ main.cc
@@ -16,13 +16,34 @@ int main(int argc, char **argv)
     );
   printf("AlN: Energy vs crystal field splitting\n");
   ham.set_crystal_parameter(_d1,105);
-  for(double i = ham.get_crystal_parameter(_d1); i < 385;
+  for(double i = ham.get_crystal_parameter(_d1); i < 386;
       i = ham.get_crystal_parameter(_d1))
     {
       printf("%.1f: %f\n", i, ham.get_eval(0));
       i = ham.get_crystal_parameter(_d1);
-      ham.set_crystal_parameter(_d1, i-1);
+      ham.set_crystal_parameter(_d1, i+5);
     }
+
+  ham.set_crystal(new exp_wz(AlN));
+  ham.set_impurity(new exp_LCZ(Al, Mg));
+  printf("Mg_Al in AlN: %f\n", ham.get_eval(0));
+
+  ham.set_crystal(new exp_wz(GaN));
+  ham.set_impurity(new exp_LCZ(Ga, Mg));
+  printf("Mg_Ga in GaN: %f\n", ham.get_eval(0));
+
+  ham.set_impurity(new exp_LCZ(Ga, Zn));
+  printf("Zn_Ga in GaN (3d as core): %f\n", ham.get_eval(0));
+
+  ham.set_impurity(new exp_LCZ(Ga_val, Zn));
+  printf("Zn_Ga in GaN (3d as valance): %f\n", ham.get_eval(0));
+
+  ham.set_crystal(new exp_wz(InN));
+  ham.set_impurity(new exp_LCZ(Ga, Zn));
+  printf("Zn_Ga in InN (3d as core): %f\n", ham.get_eval(0));
+
+  ham.set_impurity(new exp_LCZ(Ga_val, Zn));
+  printf("Zn_Ga in InN (3d as valance): %f\n", ham.get_eval(0));
 
   return 0;
 }
